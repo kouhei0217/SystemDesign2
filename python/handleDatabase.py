f071714d065b3f1f3f8ec5afb5ec25d17d1d76a5
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-11-13 21:25:46 +0900

    fix

--- python/handleDatabase.py
+++ python/handleDatabase.py
@@ -5,15 +5,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-connection = pymysql.connect(host="localhost",
-                             user="orikami",
-                             password=os.getenv("password"),
-                             db="SystemDesign2",
-                             charset="utf8mb4",
-                             cursorclass=pymysql.cursors.DictCursor)
+
+def startConnection():
+    return pymysql.connect(host="localhost",
+                           user="orikami",
+                           password=os.getenv("password"),
+                           db="SystemDesign2",
+                           charset="utf8mb4",
+                           cursorclass=pymysql.cursors.DictCursor)
 
 
 def fetchMenus():
+    connection = startConnection
     try:
         with connection.cursor() as cursor:
             sql = "SELECT menus.menu_id, menus.menu_name FROM menus NATURAL JOIN votes WHERE votes.default_flag = 1 AND votes.year = YEAR (CURRENT_TIMESTAMP) AND votes.month = MONTH (CURRENT_TIMESTAMP);"
@@ -25,6 +28,7 @@ def fetchMenus():
 
 
 def voteMenu(menuID):
+    connection = startConnection
     try:
         with connection.cursor() as cursor:
             sql = "INSERT INTO votes (year, month, menu_id, default_flag) VALUES (YEAR (CURRENT_TIMESTAMP), MONTH (CURRENT_TIMESTAMP), %s, FALSE);"
