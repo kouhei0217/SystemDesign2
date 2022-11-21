7a07067f9fadb29041fd6e6297d03a24a36eb668
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-11-22 01:58:00 +0900

    test

--- run.py
+++ run.py
@@ -65,6 +65,8 @@ def CallVoteMenu():
     expires = int(datetime.datetime.now().timestamp()) + maxAge
     value = {"hasVoted": True, "menu_id": request.json["menu_id"]}
     response.set_cookie("hasVoted", value="test")
+    cookie = request.cookies.get("hasVoted", None)
+    print(json.loads(cookie))
     # response.set_cookie("hasVoted", value=json.dumps(value),
     #                     max_age=maxAge, expires=expires)
     return response
