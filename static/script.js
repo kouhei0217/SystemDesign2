06bb08454cec13f886add74f3dea4e7765fb5bf2
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-11-24 04:19:57 +0900

    fix

--- static/script.js
+++ static/script.js
@@ -4,6 +4,7 @@ checkCookie();
 function checkCookie() {
   if (document.cookie != "") {
     elements = document.getElementsByName("menus").disabled = true;
+    console.log(elements);
     for (let i = 0; i < elements.length; i++) {
       elements.item(i).disabled = true;
       console.log(elements.item(i));
