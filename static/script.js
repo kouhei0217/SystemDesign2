93ffc1d7dc63569c8804ea876983b259a5a2d642
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-11-24 04:24:37 +0900

    fix

--- static/script.js
+++ static/script.js
@@ -4,11 +4,12 @@ checkCookie();
 function checkCookie() {
   if (document.cookie != "") {
     elements = document.getElementsByName("menus");
+    console.log(elements);
     for (let i = 0; i < elements.length; i++) {
       elements.item(i).disabled = true;
       console.log(elements.item(i));
     }
     document.getElementById("vote_menu").disabled = true;
-    document.getElementById(cookie[1]).checked = true;
+    document.getElementById(document.cookie[1]).checked = true;
   }
 }
