6ec52bac119257bb0ae49d9315e39c51a36ac6f7
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-11-24 04:17:56 +0900

    test

--- static/script.js
+++ static/script.js
@@ -6,9 +6,11 @@ function checkCookie() {
     elements = document.getElementsByName("menus").disabled = true;
     for (let i = 0; i < elements.length; i++) {
       elements.item(i).disabled = true;
+      console.log(elements.item(i));
     }
     document.getElementById("vote_menu").disabled = true;
     cookie = document.cookie.split("=");
+    console.log(cookie[0]);
     if (cookie[0] == "menu_id") {
       console.log("a");
       document.getElementById(cookie[1]).checked = true;
